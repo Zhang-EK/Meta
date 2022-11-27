e3d33922d5c5347d37cae2f01fa82c2b4f3b7d52
Zhang Erkang <96480168+Zhang-EK@users.noreply.github.com>
2022-11-27 17:17:31 +0000

    20221127

--- R_course.R
+++ R_course.R
@@ -41,9 +41,87 @@ class(a)
 seq()
 rep()
 ##character 字符
+##生成一下实验编号
+a <- 1:50
+b <- c("A","B")
+b <- rep(b, each = 50)
+paste(b,a, sep = "")
+
+########数据索引########
+rivers
+class(rivers)
+rivers[1]  ###一维索引
+rivers[-1]   #负索引表示删除数据
+
+rivers[seq(1,141,2)]
+rivers[c(T,F)]
+
+rivers[rivers > mean(rivers)]
+mean(rivers)
+
+rivers[rivers == 1000] <- 1001  ###可以修改数字
+
+#######################
+x <- read.csv("Rdata/homo_length.csv")
+x <- head(x,24)
+barplot(x[,2], names.arg = x[,1])
+
+#######矩阵########
+#二维数据
+x <- 1:100
+m <- matrix(x, nrow = 25, byrow = T)
+dim(m)
+
+x <- read.csv("Rdata/heatmap.csv", header = T, row.names = 1)
+class(x)
+x <- as.matrix(x)
+heatmap(x)
+
+x <- x[,c(1:5,11:15,6:10,16:20)]   ###可以直接调整列的顺序
+
+#apply函数可以将一维的函数拓展到二维
+apply(x,1,sum)  #1是行，2是列
+apply(x,2,sd)
+
+apply(state.x77, 2, sum)
+apply(state.x77,2, mean)[2]
+
+######数据框########
+#矩阵和数据框的区别在于矩阵的内容只能是一种属性，只能都是逻辑值、数值或者是字符串，矩阵是一种特殊的数据框
+#数据每一列是一个向量，而每一行则不是
+#read读入的文件默认都是数据框
+
+library(openxlsx)
+x <- read.xlsx("Rdata/2015.xlsx", rowNames = T)
+class(x)
+
+id <- c(57,46,77,17,36,23,75,39,33)
+treatment <- rep("Treated",9)
+sex <- rep("Male",9)
+age <- c(27,29,30,32,46,58,59,59,63)
+improved <- c("some","none","none","marked","marked","marked","none","marked","none")
+x <- data.frame(id,treatment,sex,age,improved)
+
+x$City  #学会$
+
+#要牢记向量化操作
+
+##########
+x <- read.csv("Rdata/200genes.csv", stringsAsFactors = F, row.names = 1)
+y <- read.csv("Rdata/121genes.csv",stringsAsFactors = F)
+x[1,]
+dta <- na.omit(x[unique(y$gene),])
+write.csv(dta, file = "Rdata/86genes.csv")
+###########
 
 
 
 
 
 
+
+
+
+
+
+ 
